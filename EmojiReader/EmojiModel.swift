2506ed76483e38cbbea3686df551074157c1bebc
Alex1989-star <bio181@yandex.ru>
2021-06-14 12:48:16 +0300

    Added settings UI and model

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Леся on 14.06.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
+
